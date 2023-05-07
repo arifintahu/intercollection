fbe583d8cb09adc66600a25c6e4917445f642738
Miftahul Arifin <miftahul97@gmail.com>
2023-05-07 11:31:52 +0700

    feat: setup layout

---
+++ src/pages/_app.tsx
@@ -0,0 +1,17 @@
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import Layout from '@/components/Layout'
+import theme from '@/utils/theme'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ChakraProvider>
+  )
+}
+
+export default App
---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | Inter Collection</title>
+        <meta name="description" content="Home | Inter Collection" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>Inter Collection</h1>
+      </main>
+    </>
+  )
+}
